cd9bad2049428b75787156da49197360720d5d49
stijn-castelyns <stijn@u2u.be>
2024-02-22 09:41:32 +0100

    Add project files.

---
+++ Demo 2 - Native Functions/NativeFunctionDemo/Filters/MyPromptFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeFunctionDemo.Filters;
+internal class MyPromptFilter : IPromptFilter
+{
+    public void OnPromptRendered(PromptRenderedContext context)
+    {
+        Debug.WriteLine($"Rendered prompt: {context.RenderedPrompt}");
+    }
+
+    public void OnPromptRendering(PromptRenderingContext context)
+    {
+        Debug.WriteLine($"Rendering prompt for {context.Function.Name}");
+    }
+}
---
+++ Demo 2 - Native Functions/NativeFunctionDemo/Program.cs
@@ -0,0 +1,82 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.Extensions.DependencyInjection;
+using NativeFunctionDemo.Plugins;
+using NativeFunctionDemo.Filters;
+
+
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+AzureKeyCredential azureKeyCredential = new(config["AzureOpenAI:AzureKeyCredential"]);
+string deploymentName = config["AzureOpenAI:DeploymentName"];
+Uri endpoint = new Uri(config["AzureOpenAI:Endpoint"]);
+OpenAIClient openAIClient = new(endpoint, azureKeyCredential);
+
+IKernelBuilder builder = Kernel.CreateBuilder();
+
+builder.Services.AddLogging((options) =>
+{
+    options.SetMinimumLevel(LogLevel.Trace);
+    options.AddConsole();
+});
+
+builder.Services.AddAzureOpenAIChatCompletion(deploymentName, openAIClient);
+
+var kernel = builder.Build();
+
+kernel.Plugins.AddFromType<MathPlugin>();
+
+// Add filter without DI
+// kernel.PromptFilters.Add(new MyPromptFilter());
+
+// Create chat history
+ChatHistory history = [];
+
+// Retrieve the chat completion service from the kernel
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+//Console.WriteLine(await kernel.InvokePromptAsync("What is the square root of 2?"));
+
+// Enable auto function calling
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+//// Start the conversation
+while (true)
+{
+    // Get user input
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("User > ");
+    history.AddUserMessage(Console.ReadLine()!);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentsAsync(history, kernel: kernel, executionSettings: openAIPromptExecutionSettings);
+
+    // Stream the results
+    string fullMessage = "";
+    var first = true;
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    foreach (var content in result)
+    {
+        if (first)
+        {
+            Console.Write("Assistant > ");
+            first = false;
+        }
+        Console.Write(content.Content);
+        fullMessage += content.Content;
+    }
+    Console.WriteLine();
+
+    // Add the message from the agent to the chat history
+    history.AddAssistantMessage(fullMessage);
+}
